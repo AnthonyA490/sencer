23722df79ed7e393c8d641b8557df6924b48cd8c
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-10-27 10:29:10 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+function sencor () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    control.waitMicros(2)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    control.waitMicros(10)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+}
+basic.forever(function () {
+	
+})
