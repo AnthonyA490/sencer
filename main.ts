403553ebaa718fe5bbb8364e32e10d051dc5804c
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-10-28 10:38:19 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,12 @@ function sencor () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     control.waitMicros(10)
     pins.digitalWritePin(DigitalPin.P1, 0)
+    distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
 }
+let distance = 0
+basic.showIcon(IconNames.Angry)
+distance = 0
 basic.forever(function () {
-	
+    sencor()
+    basic.showNumber(distance)
 })
