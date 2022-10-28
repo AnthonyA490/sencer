3e177a01e769676553ce477a8e8335923662af80
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-10-28 11:11:00 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -21,6 +21,16 @@ function sencor () {
     control.waitMicros(10)
     pins.digitalWritePin(DigitalPin.P1, 0)
     distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
+    if (distance <= 2) {
+        left()
+    } else {
+        forward()
+    }
+}
+function forward () {
+    pins.servoSetPulse(AnalogPin.P8, 1300)
+    pins.servoSetPulse(AnalogPin.P13, 1700)
+    control.waitMicros(20000)
 }
 let distance = 0
 basic.showIcon(IconNames.Angry)
