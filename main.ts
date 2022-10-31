08d542c8798fd03f6e66fc684e922824bee388ce
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-10-31 10:19:02 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,11 @@ function right () {
     pins.servoSetPulse(AnalogPin.P13, 1700)
     control.waitMicros(20000)
 }
+function backword () {
+    pins.servoSetPulse(AnalogPin.P8, 1700)
+    pins.servoSetPulse(AnalogPin.P13, 1100)
+    control.waitMicros(20000)
+}
 function left () {
     pins.servoSetPulse(AnalogPin.P13, 1700)
     pins.servoSetPulse(AnalogPin.P8, 1700)
@@ -14,6 +19,9 @@ function left () {
     pins.servoSetPulse(AnalogPin.P13, 1300)
     control.waitMicros(20000)
 }
+input.onButtonPressed(Button.A, function () {
+    forward()
+})
 function sencor () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     control.waitMicros(2)
@@ -21,12 +29,18 @@ function sencor () {
     control.waitMicros(10)
     pins.digitalWritePin(DigitalPin.P1, 0)
     distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
-    if (distance <= 2) {
+    if (distance <= 5) {
         left()
     } else {
         forward()
     }
 }
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    backword()
+})
 function forward () {
     pins.servoSetPulse(AnalogPin.P8, 1300)
     pins.servoSetPulse(AnalogPin.P13, 1700)
